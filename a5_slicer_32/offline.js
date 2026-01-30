d000c5657ff405e17711a69fb54fe6fdfbf63b68
s43494 <s43494@bj.ac.th>
2026-01-30 08:40:21 +0700

    Add files via upload

---
+++ a5_slicer_32/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1769737341,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/fruit-sheet0.png",
+		"images/block-sheet0.png",
+		"images/gunfruit-sheet0.png",
+		"images/splath-sheet0.png",
+		"images/spritefont.png",
+		"images/explode-sheet0.png",
+		"images/fruit2-sheet0.png",
+		"images/fruit3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
